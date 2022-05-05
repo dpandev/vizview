53dad362588e42d4e855d84cc7ca7ff2cdb31913
David Panesiu <davidpanesiu@gmail.com>
2022-05-05 08:00:27 -0600

    Removed unused/inactive navigation routes

--- src/screens/Home/HomeStackScreen.js
+++ src/screens/Home/HomeStackScreen.js
@@ -1,17 +1,23 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from './HomeScreen'
-import LoginForm from '../AuthScreens/LoginForm'
-import RegisterForm from '../AuthScreens/RegisterForm'
 import VisitorFormScreen from '../VisitorFormScreen'
 import CheckinScreen from '../CheckinScreen'
+import SignInScreen from '../SignInScreen'
+import SignUpScreen from '../SignUpScreen'
+import NewPasswordScreen from '../NewPasswordScreen'
+import ConfirmEmailScreen from '../ConfirmEmailScreen'
+import ForgotPasswordScreen from '../ForgotPasswordScreen'
 
 export default function HomeStackScreen() {
   const HomeStack = createNativeStackNavigator()
   return (  //TODO: logic for if user logged in, also if checkin-guest user logged in then show checkin screen
     <HomeStack.Navigator>
       <HomeStack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
-      <HomeStack.Screen name="LoginForm" component={LoginForm} />
-      <HomeStack.Screen name="RegisterForm" component={RegisterForm} />
+      <HomeStack.Screen name="SignInScreen" component={SignInScreen} options={{ headerShown: false }}/>
+      <HomeStack.Screen name="SignUpScreen" component={SignUpScreen} options={{ headerShown: false }}/>
+      <HomeStack.Screen name="ConfirmEmailScreen" component={ConfirmEmailScreen} options={{ headerShown: false }}/>
+      <HomeStack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} options={{ headerShown: false }}/>
+      <HomeStack.Screen name="NewPasswordScreen" component={NewPasswordScreen} options={{ headerShown: false }}/>
       <HomeStack.Screen name="VisitorFormScreen" component={VisitorFormScreen} />
       <HomeStack.Screen name="CheckinScreen" component={CheckinScreen} options={{ headerShown: false }}/>
     </HomeStack.Navigator>
--- src/screens/TestScreenStack/TestStackScreen.js
+++ src/screens/TestScreenStack/TestStackScreen.js
@@ -2,12 +2,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TestScreen from './TestScreen';
 import SignInScreen from '../SignInScreen';
 import SignUpScreen from '../SignUpScreen';
-import LoginForm from '../AuthScreens/LoginForm';
-import RegisterForm from '../AuthScreens/RegisterForm';
 import CheckinScreen from '../CheckinScreen';
 import VisitorFormScreen from '../VisitorFormScreen';
 import NotificationScreen from '../NotificationScreen';
-import HomeStackScreen from '../Home/HomeStackScreen';
 import SettingsStackScreen from '../SettingsScreen/SettingsStackScreen';
 import ConfirmEmailScreen from '../ConfirmEmailScreen';
 import ForgotPasswordScreen from '../ForgotPasswordScreen';
@@ -23,12 +20,9 @@ export default function TestStackScreen() {
       <TestStack.Screen name="ConfirmEmailScreen" component={ConfirmEmailScreen} options={{ headerShown: false }} />
       <TestStack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} options={{ headerShown: false }} />
       <TestStack.Screen name="NewPasswordScreen" component={NewPasswordScreen} options={{ headerShown: false }} />
-      <TestStack.Screen name="LoginForm" component={LoginForm} />
-      <TestStack.Screen name="RegisterForm" component={RegisterForm} />
       <TestStack.Screen name="CheckinScreen" component={CheckinScreen} />
       <TestStack.Screen name="VisitorFormScreen" component={VisitorFormScreen} options={{ headerShown: false }} />
       <TestStack.Screen name="NotificationScreen" component={NotificationScreen} />
-      <TestStack.Screen name="HomeStackScreen" component={HomeStackScreen} options={{ headerShown: false }} />
       <TestStack.Screen name="SettingsStackScreen" component={SettingsStackScreen} options={{ headerShown: false }} />
     </TestStack.Navigator>
   );
