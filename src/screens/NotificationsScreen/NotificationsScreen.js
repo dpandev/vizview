ead0165519c3d358dc2c5b42ab44a344893e85a3
Ruben Cristea <39315168+rcristea@users.noreply.github.com>
2022-05-15 14:29:13 -0400

    Finished notification screen

--- src/screens/NotificationsScreen/NotificationsScreen.js
+++ src/screens/NotificationsScreen/NotificationsScreen.js
@@ -4,163 +4,15 @@ import CustomButton from '../../components/CustomButton/CustomButton'
 import { Text } from 'react-native-paper'
 import { db, auth } from '../../../firebase'
 
-const wait = (timeout) => {
-  return new Promise(resolve => setTimeout(resolve, timeout))
+function delay(timeout) {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout)
+  })
 }
 
 const NotificationsScreen = () => {
-  /*
-  const [refreshing, setRefreshing] = useState(false)
-  const [guestList, setGuestList] = useState([])
-
-
-  const onRefresh = useCallback(() => {
-    console.log('RSM. notifications')
-    setRefreshing(true)
-    // wait(2000).then(() => setRefreshing(false))
-    let mounted = true
-    const getNotifications = async () => {
-      let list = []
-      console.log('operation begin')
-      await db.collection('barbers')
-        .where('email', '==', auth.currentUser.email)
-        .collection('checkins').get()
-        .catch((error) => console.log(error))
-        .then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            list.push({
-              ...doc.data(),
-              name: doc.name,
-              comments: doc.comment,
-              time: doc.createdAt,
-            }),
-            console.log('list was created')
-          })
-        })
-        .then(() => {
-          console.log('mounted?:', mounted)
-          if (mounted) {
-            setGuestList(list)
-            setRefreshing(false)
-          }
-        })
-        console.log('operation ran')
-    }
-    console.log('before the function')
-    console.log("data: => ", db.collection('barbers').where('email', '==', auth.currentUser.email))
-    getNotifications().catch(() => setRefreshing(false), console.log('caught something'))
-    return () => {
-      mounted = false
-      console.log('unmounting')
-      setRefreshing(false)
-    }
-  }, [])
-
-  const notifications = {
-    not1: {
-      name: 'Bob',
-      barber: 'barber1',
-      comments: 'new style',
-      time: '10:34pm',
-    },
-    not2: {
-      name: 'James',
-      barber: 'barber2',
-      comments: null,
-      time: '12:45pm',
-    },
-    not3: {
-      name: 'Alan',
-      barber: 'barber1',
-      comments: null,
-      time: '9:00am',
-    },
-    not4: {
-      name: 'James',
-      barber: 'barber2',
-      comments: null,
-      time: '12:45pm',
-    },
-    not5: {
-      name: 'Alan',
-      barber: 'barber1',
-      comments: 'new style',
-      time: '9:00am',
-    },
-    not6: {
-      name: 'James',
-      barber: 'barber2',
-      comments: 'new style',
-      time: '12:45pm',
-    },
-    not7: {
-      name: 'Alan',
-      barber: 'barber1',
-      comments: null,
-      time: '9:00am',
-    },
-    not8: {
-      name: 'James',
-      barber: 'barber2',
-      comments: 'new style',
-      time: '12:45pm',
-    },
-    not9: {
-      name: 'Alan',
-      barber: 'barber1',
-      comments: 'new style',
-      time: '9:00am',
-    },
-    not10: {
-      name: 'James',
-      barber: 'barber2',
-      comments: null,
-      time: '12:45pm',
-    },
-    not11: {
-      name: 'Alan',
-      barber: 'barber1',
-      comments: 'new style',
-      time: '9:00am',
-    },
-    not7: {
-      name: 'Alan',
-      barber: 'barber1',
-      comments: null,
-      time: '9:00am',
-    },
-    not8: {
-      name: 'James',
-      barber: 'barber2',
-      comments: null,
-      time: '12:45pm',
-    },
-    not9: {
-      name: 'Alan',
-      barber: 'barber1',
-      comments: 'new style',
-      time: '9:00am',
-    },
-    not10: {
-      name: 'James',
-      barber: 'barber2',
-      comments: null,
-      time: '12:45pm',
-    },
-    not11: {
-      name: 'Alan',
-      barber: 'barber1',
-      comments: 'new style',
-      time: '9:00am',
-    },
-  }
-
-  const handleClick = () => {
-    console.log("notification clicked")
-  }
-
-  */
   const [notifications, setNotifications] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     loadNotifications()
@@ -186,23 +38,31 @@ const NotificationsScreen = () => {
         tn.time =  dateTime.toLocaleString('en-US', { timeStyle: 'short', hour12: true })
       })
 
-      setNotifications(tempNotifications.reverse())
+      const sorted = tempNotifications.sort((a, b) => {
+        const aDate = new Date(a.createdAt.seconds * 1000)
+        const bDate = new Date(b.createdAt.seconds * 1000)
+        return bDate - aDate
+      })
+
+      setNotifications(sorted)
     })
   }
   
+  const refresh = useCallback(async () => {
+    setLoading(true)
+    loadNotifications().then(() => setLoading(false))
+  }, [loading])
 
   return (
-    // <ScrollView 
-    //   refreshControl={
-    //     <RefreshControl 
-    //       refreshing={refreshing} 
-    //       onRefresh={onRefresh} 
-    //       contentContainerStyle={{flexGrow: 1}} 
-    //     />
-    //   }>
-    <ScrollView>
+    <ScrollView refreshControl={
+      <RefreshControl
+        progressBackgroundColor={"black"}
+        tintColor="black"
+        refreshing={loading}
+        onRefresh={refresh}
+      />
+    }>
       <View style={styles.container}>
-        <CustomButton onPress={debugGetNotifications} text={"debugGetNotifications"} />
         {Object.values(notifications).map((item, id) => (
           <Text key={id} style={styles.button}>
             <Text style={styles.text}>{item.name} has checked in at {item.time}</Text>
@@ -211,7 +71,6 @@ const NotificationsScreen = () => {
             </Text>
           </Text>
         ))}
-        <Text>hi</Text>
       </View>
     </ScrollView>
   )
@@ -244,6 +103,11 @@ const styles = StyleSheet.create({
   noDisplay: {
     display: 'none',
   },
+  scrollView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 })
 
 export default NotificationsScreen
